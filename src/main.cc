d1be41bb6fc0d9b12c8e224eb83ee327e1b48653
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-02-19 17:30:22 +0100

    [MEDIA] Base sprites added

--- src/main.cc
+++ src/main.cc
@@ -3,16 +3,18 @@
 int main(void)
 {
 
+  /* Init */
   if (SDL_Init(SDL_INIT_EVERYTHING))
   {
     SDL_Log("Unable to initialize SDL : %s", SDL_GetError());
     return 1;
   }
 
-  SDL_Window *window = SDL_CreateWindow(
-    "My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-    800, 500,
-    0);
+  /* Window */
+  SDL_Window* window = SDL_CreateWindow(
+      "My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+      800, 500,
+      0);
 
   if (!window)
   {
@@ -20,7 +22,34 @@ int main(void)
     return 1;
   }
 
-  SDL_Delay(3000);
+  /* Renderer */
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+  /* Surface */
+  SDL_Surface* surface = SDL_LoadBMP("media/bar.bmp");
+  if (!surface)
+  {
+    SDL_Log("Unable to load surface : %s", SDL_GetError());
+    return 1;
+  }
+
+  /* Texture */
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+  bool running = true;
+
+  while (running)
+  {
+    SDL_Event event;
+    if (SDL_PollEvent(&event))
+    {
+      if (event.key.keysym.sym == SDLK_ESCAPE)
+        running = false;
+    }
+
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+  }
 
   SDL_DestroyWindow(window);
   SDL_Quit();
